0505106a3ae41d6d647624cc5f5a5760444472d5
gawhite <gawhite@users.noreply.github.com>
2025-03-03 16:39:51 -0500

    Fix Tailwind classname order

--- src/components/employee-feature-card.tsx
+++ src/components/employee-feature-card.tsx
@@ -1,32 +1,25 @@
 import { CarouselItem } from "@/components/ui/carousel";
 import { Card, CardContent } from "@/components/ui/card";
 import { cn } from "@/lib/utils";
+import Image from "next/image";
 
 interface EmployeeFeatureCardProps {
   title: string;
   description: string;
-  icon: React.ForwardRefExoticComponent<
-    Omit<LucideProps, "ref"> & React.RefAttributes<SVGSVGElement>
-  >;
   className?: string;
 }
 
-export function EmployeeFeatureCard({
-  title,
-  description,
-  icon: Icon,
-  className,
-}: EmployeeFeatureCardProps) {
+export function EmployeeFeatureCard({ title, description, className }: EmployeeFeatureCardProps) {
   return (
     <CarouselItem className={cn("md:basis-1/2 lg:basis-1/3", className)}>
       <div className="h-full p-1">
-        <Card className="h-full shadow-md text-card">
+        <Card className="h-full text-card shadow-md">
           <CardContent className="flex flex-col items-start gap-5 p-7">
             <div className="inline-flex items-center justify-center rounded-md border border-border p-2">
-              <img
+              <Image
                 alt="Image of Employee"
                 src="/images/company-page-geoff.webp"
-                height={0}
+                height={300}
                 width={300}
               />
             </div>
--- src/components/vit-feature-card.tsx
+++ src/components/vit-feature-card.tsx
@@ -1,5 +1,6 @@
 import { Card, CardContent } from "@/components/ui/card";
 import { cn } from "@/lib/utils";
+import { LucideProps } from "lucide-react";
 
 interface VitFeatureCardProps {
   title: string;
@@ -25,9 +26,7 @@ export function VitFeatureCard({
       <CardContent className="flex flex-col items-start gap-5 p-7">
         <div className="inline-flex items-center justify-center rounded-md border border-border p-2 text-center h-14 w-14 bg-muted-foreground/30">
           <h2 className="text-muted-foreground text-3xl">{letter1}</h2>
-          <h2 classname="text-primary" className="text-3xl text-accent">
-            {letter2}
-          </h2>
+          <h2 className="text-3xl text-accent">{letter2}</h2>
         </div>
         <div>
           <h4 className="mb-2 text-lg font-semibold text-card-foreground">{title}</h4>
