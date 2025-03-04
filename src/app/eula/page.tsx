5e7e20e0979d9764fe63685ea9b081569c51ce4d
gawhite <gawhite@users.noreply.github.com>
2025-03-04 17:00:55 -0500

    Build Out Termageddon EULA Page

--- src/app/eula/page.tsx
+++ src/app/eula/page.tsx
@@ -1,11 +1,25 @@
-// EULA Page section-12.tsx //
+// EULA Page //
 
-import { Section12 } from "@/components/section-12";
+'use client';
 
-export default function EulaPage() {
+import { VitHeader } from "@/components/vit-header";
+import { VitBreadcrumbs } from "@/components/vit-breadcrumbs";
+import { VitFooter } from "@/components/vit-footer";
+import type { NextPage } from 'next';
+import Policy from '../../components/shared/Policy';
+
+const EulaPage: NextPage = () => {
   return (
-    <>
-      <Section12 />
-    </>
+    <main className="p-4 bg-background">
+    <VitHeader />
+    <VitBreadcrumbs label="EULA" href="/eula" />
+    <div className="container border border-border rounded-md mb-8 py-10" >
+      <h2 className="text-accent text-3xl font-bold">EULA</h2>
+     <Policy policyKey="V1RRclpYVnFWR0Z1ZGxvcmJIYzlQUT0"/>
+    </div>
+    <VitFooter/>
+  </main>
   );
-}
+};
+
+export default EulaPage;
