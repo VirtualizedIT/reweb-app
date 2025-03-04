9585ad0a56c2579e2f78dcf24b418ce74f4dd286
gawhite <gawhite@users.noreply.github.com>
2025-03-04 16:58:48 -0500

    Build out Termageddon Disclaimer Page

--- src/app/disclaimer/page.tsx
+++ src/app/disclaimer/page.tsx
@@ -1,11 +1,25 @@
-// Disclaimer Page section-2.tsx //
+// Disclaimer Page //
 
-import { Section2 } from "@/components/section-2";
+'use client';
 
-export default function DisclaimerPage() {
+import { VitHeader } from "@/components/vit-header";
+import { VitBreadcrumbs } from "@/components/vit-breadcrumbs";
+import { VitFooter } from "@/components/vit-footer";
+import type { NextPage } from 'next';
+import Policy from '../../components/shared/Policy';
+
+const DisclaimerPage: NextPage = () => {
   return (
-    <>
-      <Section2 />
-    </>
+    <main className="p-4 bg-background">
+    <VitHeader />
+    <VitBreadcrumbs label="Disclaimer" href="/disclaimer" />
+    <div className="container border border-border rounded-md mb-8 py-10" >
+      <h2 className="text-accent text-3xl font-bold">Disclaimer</h2>
+      <Policy policyKey="UVZGaFZWWklZWFZsWkVsd1pYYzlQUT09"/>
+    </div>
+    <VitFooter/>
+  </main>
   );
-}
+};
+
+export default DisclaimerPage;
