1c60e27fe4368520771b0f8fe9f75c96552579cb
gawhite <gawhite@users.noreply.github.com>
2025-03-04 16:55:35 -0500

    Build Out Cookie Page

--- src/app/cookie/page.tsx
+++ src/app/cookie/page.tsx
@@ -1,11 +1,25 @@
-// Cookie Page section-11.tsx //
+// Cookie Page //
 
-import { Section11 } from "@/components/section-11";
+'use client';
 
-export default function CookiePage() {
+import { VitHeader } from "@/components/vit-header";
+import { VitBreadcrumbs } from "@/components/vit-breadcrumbs";
+import { VitFooter } from "@/components/vit-footer";
+import type { NextPage } from 'next';
+import Policy from '../../components/shared/Policy';
+
+const CookiePage: NextPage = () => {
   return (
-    <>
-      <Section11 />
-    </>
+    <main className="p-4 bg-background">
+    <VitHeader />
+    <VitBreadcrumbs label="Cookie" href="/cookie" />
+    <div className="container border border-border rounded-md mb-8 py-10" >
+      <h2 className="text-accent text-3xl font-bold">Cookie Policy</h2>
+      <Policy policyKey="UVhSUGFVdE5VWE12UVVwTFNGRTlQUT09" />
+    </div>
+    <VitFooter/>
+  </main>
   );
-}
+};
+
+export default CookiePage;
